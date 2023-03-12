25983b9189468345b06d62a063c780779333385a
manoflogan <manoflogan@gmail.com>
2023-03-11 17:36:38 -0800

    [Step 17] Modelling user behaviour.

---
+++ app/src/main/java/com/manoflogan/compose/authentication/AuthenticationEvent.kt
@@ -0,0 +1,10 @@
+package com.manoflogan.compose.authentication
+
+sealed class AuthenticationEvent {
+
+    object ToggleAuthenticationEvent: AuthenticationEvent()
+
+    class EmailChangedEvent(val email: String): AuthenticationEvent()
+
+    class PasswordChangedEvent(val password: String): AuthenticationEvent()
+}
--- app/src/main/java/com/manoflogan/compose/authentication/AuthenticationViewModel.kt
+++ app/src/main/java/com/manoflogan/compose/authentication/AuthenticationViewModel.kt
@@ -1,12 +1,56 @@
 package com.manoflogan.compose.authentication
 
 import androidx.lifecycle.ViewModel
+import com.manoflogan.compose.authentication.models.AuthenticationMode
 import com.manoflogan.compose.authentication.models.AuthenticationState
+import com.manoflogan.compose.authentication.models.PasswordRequirements
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class AuthenticationViewModel: ViewModel() {
 
     val _authenticationStateFlow= MutableStateFlow(AuthenticationState())
     val authenticationStateFlow = _authenticationStateFlow.asStateFlow()
+
+    fun toggleAuthenticationMode() {
+        val authenticationState = _authenticationStateFlow.value
+        val toggledAuthenticationMode =
+            if (authenticationState.authenticationMode == AuthenticationMode.SIGN_IN) {
+                AuthenticationMode.SIGN_UP
+            } else {
+                AuthenticationMode.SIGN_UP
+            }
+        _authenticationStateFlow.value =
+            _authenticationStateFlow.value.copy(authenticationMode = toggledAuthenticationMode)
+    }
+    fun updateEmail(email: String) {
+        _authenticationStateFlow.value = _authenticationStateFlow.value.copy(email = email)
+    }
+
+    fun updatePassword(password: String) {
+        val requirements = mutableListOf<PasswordRequirements>()
+        if (password.length > 7) {
+            requirements.add(PasswordRequirements.EIGHT_CHARACTERS)
+        }
+        if (password.any { it.isUpperCase() }) {
+            requirements.add(PasswordRequirements.CAPITAL_LETTERS)
+        }
+        if (password.any { it.isDigit() }) {
+            requirements.add(PasswordRequirements.NUMBER)
+        }
+        _authenticationStateFlow.value = _authenticationStateFlow.value.copy(
+            password = password, passwordRequirements = requirements
+        )
+    }
+
+    fun handleAuthenticationEvent(authenticationEvent: AuthenticationEvent) {
+        if (authenticationEvent is AuthenticationEvent.ToggleAuthenticationEvent) {
+            toggleAuthenticationMode()
+        } else if (authenticationEvent is AuthenticationEvent.EmailChangedEvent) {
+            updateEmail(authenticationEvent.email)
+        } else if (authenticationEvent is AuthenticationEvent.PasswordChangedEvent) {
+            updatePassword(authenticationEvent.password)
+        }
+    }
 }
