c8d0cede4fabd70ba3698abeb06a18c86384f0d7
manoflogan <manoflogan@gmail.com>
2023-05-29 21:15:32 -0700

    Destination.

--- home/src/main/java/com/manoflogan/compose/home/Destination.kt
+++ home/src/main/java/com/manoflogan/compose/home/Destination.kt
@@ -1,11 +1,38 @@
 package com.manoflogan.compose.home
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.ui.graphics.vector.ImageVector
 
+/**
+ * Sealed destination representing the destination for drawer layout, bottom navigation destination,
+ * and floating action button navigation.
+ */
 sealed class Destination(
     val path: String,
     val imageVector: ImageVector? = null
 ) {
 
-    
+    /**
+     * Home destination
+     */
+    object Home: Destination("home")
+
+    /**
+     * Feed
+     */
+    object Feed: Destination("Feed", Icons.Default.List)
+
+    /**
+     * Calendar
+     */
+    object Calendar: Destination("Calendar", Icons.Default.DateRange)
+
+    /**
+     * Contacts
+     */
+    object Contacts: Destination("contacts", Icons.Default.Person)
 }
