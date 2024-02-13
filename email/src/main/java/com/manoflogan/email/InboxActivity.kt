f15d8218ba2ba991e253b52ed56e7fe8e3ea5d4a
manoflogan <manoflogan@gmail.com>
2024-02-14 00:09:53 +0530

    Removing the greeting composable.

--- email/src/main/java/com/manoflogan/email/InboxActivity.kt
+++ email/src/main/java/com/manoflogan/email/InboxActivity.kt
@@ -5,12 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.manoflogan.email.composables.Inbox
 import com.manoflogan.email.ui.theme.JetpackComposeAuthenticationTheme
 
@@ -20,26 +15,12 @@ class InboxActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Inbox(
-                modifier = Modifier.fillMaxSize(),
-                viewModel = inboxViewModel
-            )
+            JetpackComposeAuthenticationTheme {
+                Inbox(
+                    modifier = Modifier.fillMaxSize(),
+                    viewModel = inboxViewModel
+                )
+            }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    JetpackComposeAuthenticationTheme {
-        Greeting("Android")
     }
 }
