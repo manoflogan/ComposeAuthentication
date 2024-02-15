9695f562f72c6e6d3f0f8e0ae29d8e8c69a6b5e9
manoflogan <manoflogan@gmail.com>
2024-02-15 14:43:21 +0530

    Using ListItem instead.

--- email/src/main/java/com/manoflogan/email/composables/EmailContent.kt
+++ email/src/main/java/com/manoflogan/email/composables/EmailContent.kt
@@ -1,14 +1,20 @@
 package com.manoflogan.email.composables
 
+import android.net.ipsec.ike.IkeSessionParams.IkeAuthDigitalSignRemoteConfig
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Email
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SwipeToDismissBox
 import androidx.compose.material3.SwipeToDismissBoxState
@@ -17,6 +23,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -57,12 +64,17 @@ fun EmailContent(
             label = "card_elevation"
         )
         Card(modifier = modifier.then(Modifier.fillMaxWidth()), elevation = CardDefaults.cardElevation(cardElevation)) {
-            Column(modifier = Modifier.fillMaxWidth()) {
-                Text(text = email.title, style = MaterialTheme.typography.headlineSmall)
-                Spacer(modifier = Modifier.height(dimensionResource(id = R.dimen.email_padding_half)))
-                Text(text = email.description, style = MaterialTheme.typography.bodyMedium,
-                    maxLines = 2, overflow = TextOverflow.Ellipsis)
-            }
+            ListItem(
+                modifier = Modifier.fillMaxWidth(),
+                headlineContent = {
+                    Text(text = email.title, style = MaterialTheme.typography.headlineSmall)
+                },
+                supportingContent = {
+                    Text(text = email.description, style = MaterialTheme.typography.bodyMedium,
+                        maxLines = 2, overflow = TextOverflow.Ellipsis
+                    )
+                }
+            )
         }
     }
 }
