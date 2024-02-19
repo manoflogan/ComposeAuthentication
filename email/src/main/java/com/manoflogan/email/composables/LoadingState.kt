16cd4fe5022207b9f0402435a6693b6abb9a8f82
manoflogan <manoflogan@gmail.com>
2024-02-20 01:52:14 +0530

    LoadingStatus test.

--- email/src/main/java/com/manoflogan/email/composables/LoadingState.kt
+++ email/src/main/java/com/manoflogan/email/composables/LoadingState.kt
@@ -7,13 +7,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.dimensionResource
 import com.manoflogan.email.R
 
 @Composable
 fun LoadingStatus(modifier: Modifier = Modifier) {
     Row(
-        modifier = modifier,
+        modifier = modifier.then(Modifier.testTag(LoadingStateTags.TAG)),
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically
     ) {
@@ -22,4 +23,8 @@ fun LoadingStatus(modifier: Modifier = Modifier) {
             strokeWidth = dimensionResource(id = R.dimen.stroke_width)
         )
     }
+}
+
+object LoadingStateTags {
+    const val TAG = "LoadingTag"
 }
--- email/src/main/java/com/manoflogan/email/composables/SemanticProperties.kt
+++ email/src/main/java/com/manoflogan/email/composables/SemanticProperties.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class, ExperimentalMaterial3Api::class)
+
 package com.manoflogan.email.composables
 
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -5,7 +7,5 @@ import androidx.compose.material3.SwipeToDismissBoxValue
 import androidx.compose.ui.semantics.SemanticsPropertyKey
 import androidx.compose.ui.semantics.SemanticsPropertyReceiver
 
-@OptIn(ExperimentalMaterial3Api::class)
-val SwipeDismissStateProperty = SemanticsPropertyKey<SwipeToDismissBoxValue>("colour")
-@OptIn(ExperimentalMaterial3Api::class)
+val SwipeDismissStateProperty = SemanticsPropertyKey<SwipeToDismissBoxValue>("swipeToDismissProperty")
 var SemanticsPropertyReceiver.swipeDismissStateId by SwipeDismissStateProperty
